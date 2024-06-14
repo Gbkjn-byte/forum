48cbff4c54d46706862da5a3e27404cddafa90fc
Glotsky Nick <tuktuk1254@gmail.com>
2024-06-14 18:51:46 +0300

    third nickname POST commit

--- post.php
+++ post.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['message']) && !empty($_POST['chat_id'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['message']) && !empty($_POST['chat_id']) && !empty($_POST['nickname'])) {
     if (!empty($_POST['nickname'])) {
         $_SESSION['nickname'] = htmlspecialchars($_POST['nickname'], ENT_QUOTES, 'UTF-8');
     }
-    $nickname = $_SESSION['nickname'];
+    $nickname = $_POST['nickname']
     $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
     $chat_id = intval($_POST['chat_id']);
     $class = 'user'; // Все сообщения отправляются пользователем
